4df79cf91103c8061dd4b2029fe3c7f1b9474886
John Creech <johnwaynecreech1@gmail.com>
2026-04-28 17:47:35 -0400

    Final fix

--- card-inventory/src/environments/environment.prod.ts
+++ card-inventory/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@
 export const environment = {
   production: true,
   bypassAuth: false,
-  justTcgApiKey: 'YOUR_JUSTTCG_API_KEY',
-  supabaseUrl: 'YOUR_SUPABASE_URL',
-  supabaseAnonKey: 'YOUR_SUPABASE_ANON_KEY',
+  justTcgApiKey: 'tcg_3b59c0a16e5b4bd384f0c2ec2e9c2a9c',
+  supabaseUrl: 'https://utlptclknauqxzhgymsd.supabase.co',
+  supabaseAnonKey: 'sb_publishable_nygQBRM8gUYEn-W158WaJQ_MkPOHyyG',
 };
